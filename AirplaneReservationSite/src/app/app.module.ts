381125ef8845022a62544f312edac0e3c6fcc777
Harshitwat <harshit.watvani@lntinfotech.com>
2020-08-12 16:39:01 +0530

    userRegisteration component

--- AirplaneReservationSite/src/app/app.module.ts
+++ AirplaneReservationSite/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsConfirmationComponent } from './details-confirmation/details-confirmation.component';
+import { UserRegisterationComponent } from './user-registeration/user-registeration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsConfirmationComponent,
+    UserRegisterationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
